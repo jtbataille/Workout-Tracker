e22b8483647dd27948e9632e05a1f0fc3817fd5c
Jarrod Bataille <jtbataille@gmail.com>
2020-10-19 21:51:35 -0400

    deploying

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
