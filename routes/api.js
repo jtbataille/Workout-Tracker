6da1897b38db73d08533ae5d2265e6352c2f364a
Jarrod Bataille <jtbataille@gmail.com>
2020-10-19 21:25:36 -0400

    added API route to change previous workout

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
+// Creates a brand new workout
 router.post("/api/workouts", (req, res) => {
   Workout.create(req.body)
     .then(dbWorkout =>{
@@ -11,18 +12,30 @@ router.post("/api/workouts", (req, res) => {
     });
 });
 
+// Finds workout by ID and updates that workout
 router.put("/api/workouts/:id", ({ body, params }, res) => {
-  Workout.findByIdAndUpdate();
+  Workout.findByIdAndUpdate(
+    params.id,
+      { $push: { exercises: body } },
+      { new: true }
+  )
+  .then(dbWorkout => res.json(dbWorkout))
+  .catch((err) => {
+    res.status(400).json(err);
+  });
 });
 
+// Finds the last workout
 router.get("/api/workouts", (req, res) => {
   Workout.find();
 });
 
+// Pulls all workouts and makes charts
 router.get("/api/workouts/range", (req, res) => {
   Workout.find({}).limit(7);
 });
 
+// Finds workout and deletes it
 router.delete("/api/workouts", ({ body }, res) => {
   Workout.findByIdAndDelete(body.id);
 });
